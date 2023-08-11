5fb3ac2fed7a526245ab43d0066341d96d369460
Luis Victoria <x@lambda.lv>
2023-08-11 18:06:17 -0400

    Add Pyright LSP to Python in Doom Emacs

--- doom/packages.el
+++ doom/packages.el
@@ -48,3 +48,11 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+(package! lsp-pyright)
+(after! lsp-mode
+  (use-package lsp-pyright
+    :ensure t
+    :hook (python-mode . (lambda ()
+                           (require 'lsp-pyright)
+                           (lsp))))) ; or lsp-deferred
