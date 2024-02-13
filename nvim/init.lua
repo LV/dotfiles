f26bd50184a7baff9f9b29ccf3d5711b4e0894fa
Luis Victoria <x@lambda.lv>
2024-02-12 23:53:03 -0500

    Add lazygit to neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -70,6 +70,12 @@ require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
   -- Git related plugins
+  {
+  "kdheepak/lazygit.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+  },
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
 
@@ -209,11 +215,11 @@ require('lazy').setup({
     config = function()
       -- your optional
 
-    require("bluloco").setup({
-      style = "dark",               -- "auto" | "dark" | "light"
+    require('bluloco').setup({
+      style = 'dark',               -- 'auto' | 'dark' | 'light'
       transparent = false,
       italics = true,
-      terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
+      terminal = vim.fn.has('gui_running') == 1, -- bluoco colors are enabled in gui terminals per default.
       guicursor   = true,
     })
 
@@ -234,7 +240,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'dark',
+        theme = 'auto',
         component_separators = '|',
         section_separators = '',
       },
@@ -438,6 +444,7 @@ end
 vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
 vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>gg', ':LazyGit<cr>', { desc = 'LazyGit' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
